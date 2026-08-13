0d4ceee6f4615c05f534cd6f50169a535d925cca
Muhammad Misbahuddin <misbahudinjoss@gmail.com>
2026-08-13 13:04:28 +0700

    fix: add secure cookie config for production II

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,45 +5,7 @@ import { prisma } from "./prisma";
 import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: true,
-      },
-    },
-    callbackUrl: {
-      name: `__Secure-next-auth.callback-url`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: true,
-      },
-    },
-    csrfToken: {
-      name: `__Host-next-auth.csrf-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: true,
-      },
-    },
-    state: {
-      name: `__Secure-next-auth.state`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: true,
-        maxAge: 900,
-      },
-    },
-  },
+  useSecureCookies: process.env.NODE_ENV === "production",
 
   providers: [
     GoogleProvider({
